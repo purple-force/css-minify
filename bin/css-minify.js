65e04869afd8ea4dd9ea84288fbfc1f56d06918f
陈连东 <chenliandong@chenliandongdeMacBook-Pro.local>
2017-04-24 20:46:05 +0800

    1.add function:resolve css source directory 2.add some command line parameters

--- bin/css-minify.js
+++ bin/css-minify.js
@@ -1,36 +1,47 @@
 #!/usr/bin/env node
 var path=require("path"),
 	fs=require("fs"),
-	minify=require("../lib/minify.js");
+	minify=require("../lib/minify.js"),
+	resolveFile=require("../lib/resolveFile.js");
 var argv=require("yargs")
+		.usage("Usage: css-minify <options> [value]")
 		.alias("v","version")
+		.describe("v","show version info")
+		.boolean("v")
 		.alias("f","file")
+		.describe("f","specify a css source file")
+		.alias("d","dir")
+		.describe("d","specify a css source directory")
+		.help("h")
+		.alias("h","help")
+		.describe("h","show help info")
 		.argv;
 var version=require("../package.json").version;
 if(argv.v){
 	console.log("css-minify "+version);
 	return;
 }
-var source=argv.f;
-if(typeof source==="string"){
-	var	filename=path.basename(source,".css"),
-		dest=path.dirname(source)+path.sep+filename+".min.css";
-	if(!path.isAbsolute(source)){
-		source=path.resolve(process.cwd(),source);
-	}
-	if(!path.isAbsolute(dest)){
-		dest=path.resolve(process.cwd(),dest);
-	}
-	fs.readFile(source,"utf8",function(err,data){
-		if(err){
-			throw new Error(source+" 读取失败!")
-		}
-		data=minify(data);
-		fs.writeFile(dest,data,function(err){
-			if(err){
-				throw new Error(dest+" 生成失败!")
-			}
-			console.log(dest+" 生成成功!")
-		})
-	})
+var file=argv.f,
+	dir=argv.d,
+	cssReg=/\.css$/;
+if(file){
+	resolveFile(path.join(process.cwd(),file));
+	return;
 }
+if(dir){
+	fs.mkdirSync(path.join(process.cwd(),"css-dist"));
+	fs.readdir(path.join(process.cwd(),dir),function(err,filenames){
+		if(err){
+			console.log(err);
+			throw new Error("读取目录"+path.join(process.cwd(),dir)+"失败")
+		}
+		filenames.forEach(function(filename){
+			if(cssReg.test(filename)){
+				resolveFile(path.join(process.cwd(),dir,filename));
+			}
+		})
+		
+	})
+	return;
+}
+
