4c732eadcaf71f784f91dcb9dfdda421283591ad
轩坊 <liandong.cld@alibaba-inc.com>
2021-08-09 14:08:15 +0800

    feat: 2.0

--- bin/css-minify.js
+++ bin/css-minify.js
@@ -1,43 +1,33 @@
 #!/usr/bin/env node
-var path = require("path"),
-  fs = require("fs"),
-  resolveFile = require("../lib/resolveFile.js");
-var argv = require("yargs")
-  .usage("Usage: css-minify <options> [value]")
-  .alias("v", "version")
-  .describe("v", "show version info")
-  .boolean("v")
-  .alias("f", "file")
-  .describe("f", "specify a css source file")
-  .alias("d", "dir")
-  .describe("d", "specify a css source directory")
-  .alias("o", "output")
-  .describe("o", "specify a minified css output directory")
-  .help("h")
-  .alias("h", "help")
-  .describe("h", "show help info").argv;
-var version = require("../package.json").version;
-if (argv.v) {
-  console.log("css-minify " + version);
-  return;
-}
-var file = argv.f,
-  dir = argv.d,
-  out = argv.o,
-  cssReg = /\.css$/;
+const path = require('path');
+const fs = require('fs');
+const { program } = require('commander');
+const resolveFile = require('../lib/resolveFile.js');
+
+const version = require('../package.json').version;
+program
+  .version(version)
+  .option('-f, --file <file>', 'specify a css source file')
+  .option('-d, --dir <dir>', 'specify a css source directory')
+  .option('-o, --output <output>', 'specify a minified css output directory')
+
+program.parse(process.argv);
+
+const options = program.opts();
+const { file, dir, output } = options;
 if (file) {
-  resolveFile(path.join(process.cwd(), file));
+  resolveFile(path.join(process.cwd(), file), output);
   return;
 }
 if (dir) {
   fs.readdir(path.join(process.cwd(), dir), function (err, filenames) {
     if (err) {
       console.log(err);
-      throw new Error("读取目录" + path.join(process.cwd(), dir) + "失败");
+      throw new Error('读取目录' + path.join(process.cwd(), dir) + '失败');
     }
     filenames.forEach(function (filename) {
-      if (cssReg.test(filename)) {
-        resolveFile(path.join(process.cwd(), dir, filename), out);
+      if (filename.endsWith('.css') && !filename.endsWith('.min.css')) {
+        resolveFile(path.join(process.cwd(), dir, filename), output);
       }
     });
   });
